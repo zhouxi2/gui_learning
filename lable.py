62091205ed5e3d33768f718e2153e4e01f411200
zhouxi2 <zhouxi2@wgc.school.nz>
2019-02-15 09:39:42 +1300

    added title and geometry to window

--- lable.py
+++ lable.py
@@ -1,10 +1,10 @@
 import tkinter
-root=tkinter.Tk()
 
-root.tiltle("Hello World!!!")
+root=tkinter.Tk()
+root.title("Hello World!!!")
 root.geometry("600x200")
 
-my_label=tkinter.Lable(root)
+my_label=tkinter.Label(root)
 my_label.config(text="Hello",fg="green")
 my_label.grid()
 
